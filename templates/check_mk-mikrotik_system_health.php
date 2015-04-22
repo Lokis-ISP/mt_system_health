d414b8c8b7ab521c3757173a30c68d707fd17ff1
Łukasz Górski <moon.musick@gmail.com>
2015-04-22 21:28:39 +0200

    Fix PNP4Nagios template
    Modified template to use PNP4Nagios helper functions;
    changed MAX to MIN in voltage graph (makes more sense);
    changed colors in temperature graph.

--- templates/check_mk-mikrotik_system_health.php
+++ templates/check_mk-mikrotik_system_health.php
@@ -9,9 +9,9 @@ foreach ($this->DS as $KEY=>$VAL) {
     $def[$i] .= rrd::def  ("voltage", $VAL['RRDFILE'], $VAL['DS'], "AVERAGE");
     $def[$i] .= rrd::area ("voltage", "#ADC7DB", "Voltage");
     $def[$i] .= rrd::line1("voltage", "#0000a8");
-    $def[$i] .= "GPRINT:voltage:LAST:\"Last %2.2lf V\" ";
-    $def[$i] .= "GPRINT:voltage:MAX:\"Max %2.2lf V\" ";
-    $def[$i] .= "GPRINT:voltage:AVERAGE:\"Average %2.2lf V\" ";
+    $def[$i] .= rrd::gprint("voltage", "LAST",    "Last %2.2lf V");
+    $def[$i] .= rrd::gprint("voltage", "MIN",     "\tMin %2.2lf V");
+    $def[$i] .= rrd::gprint("voltage", "AVERAGE", "\tAverage %2.2lf V");
   }
 }
 
@@ -25,10 +25,11 @@ foreach ($this->DS as $KEY=>$VAL) {
 
     $opt[$i] .= "--vertical-label \"degrees Celsius\" --title \"Temperature for $hostname\" ";
     $def[$i] .= rrd::def('temp', $VAL['RRDFILE'], $VAL['DS'], 'AVERAGE', 'Temperature');
-    $def[$i] .= rrd::gradient('temp', 'eeeeee', 'e8b920', 'Temperature', 50);
-    $def[$i] .= "GPRINT:temp:LAST:\"Last %2.2lf C\" ";
-    $def[$i] .= "GPRINT:temp:MAX:\"Max %2.2lf C\" ";
-    $def[$i] .= "GPRINT:temp:AVERAGE:\"Average %2.2lf C\" ";
+    $def[$i] .= rrd::gradient('temp', 'f2e7a5', 'f06f22', 'Temperature', 50);
+    $def[$i] .= rrd::line1("temp", "#852b13");
+    $def[$i] .= rrd::gprint("temp", "LAST",    "Last %2.2lf C");
+    $def[$i] .= rrd::gprint("temp", "MAX",     "\tMax %2.2lf C");
+    $def[$i] .= rrd::gprint("temp", "AVERAGE", "\tAverage %2.2lf C");
   }
 }
 
@@ -43,9 +44,9 @@ foreach ($this->DS as $KEY=>$VAL) {
     $opt[$i] .= "--vertical-label \"Watt\" --title \"Power consumption for $hostname\" ";
     $def[$i] .= rrd::def ('watt', $VAL['RRDFILE'], $VAL['DS'], 'AVERAGE');
     $def[$i] .= rrd::area('watt', $color, 'Power Consumption');
-    $def[$i] .= "GPRINT:watt:LAST:\"Last %2.2lf W\" ";
-    $def[$i] .= "GPRINT:watt:MAX:\"Max %2.2lf W\" ";
-    $def[$i] .= "GPRINT:watt:AVERAGE:\"Average %2.2lf W\" ";
+    $def[$i] .= rrd::gprint("watt", "LAST",    "Last %2.2lf W");
+    $def[$i] .= rrd::gprint("watt", "MAX",     "\tMax %2.2lf W");
+    $def[$i] .= rrd::gprint("watt", "AVERAGE", "\tAverage %2.2lf W");
   }
 }
 
@@ -60,9 +61,9 @@ foreach ($this->DS as $KEY=>$VAL) {
     $opt[$i] .= "--vertical-label \"mA\" --title \"Current for $hostname\" ";
     $def[$i] .= rrd::def ('current', $VAL['RRDFILE'], $VAL['DS'], 'AVERAGE');
     $def[$i] .= rrd::area('current', $color, 'Current');
-    $def[$i] .= "GPRINT:current:LAST:\"Last %2.0lf mA\" ";
-    $def[$i] .= "GPRINT:current:MAX:\"Max %2.0lf mA\" ";
-    $def[$i] .= "GPRINT:current:AVERAGE:\"Average %2.0lf mA\" ";
+    $def[$i] .= rrd::gprint("current", "LAST",    "Last %4.0lf mA");
+    $def[$i] .= rrd::gprint("current", "MAX",     "\tMax %4.0lf mA");
+    $def[$i] .= rrd::gprint("current", "AVERAGE", "\tAverage %4.0lf mA");
   }
 }
 ?>
